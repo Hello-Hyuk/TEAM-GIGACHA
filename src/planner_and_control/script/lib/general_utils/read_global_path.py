2adb4c97eebcd514c30a42d8635638c8c8199756
hhs4420 <hhs4420@naver.com>
2022-02-24 22:48:39 +0900

    finish mission and behavior planner
    finish mission and behavior planner

--- src/planner_and_control/script/lib/general_utils/read_global_path.py
+++ src/planner_and_control/script/lib/general_utils/read_global_path.py
@@ -1,1 +1,14 @@
-#must fix 
+import rospy
+from planner_and_control.msg import Path
+import csv
+from numpy import rad2deg
+
+def read_global_path(name):
+        global_path = Path()
+        with open("'/home/hyunswim/TEAM-GIGACHA/src/planner_and_control/script/lib/mapping_utils/maps/" + name + ".csv", mode="r") as csv_file:
+            csv_reader = csv.reader(csv_file)
+            for line in csv_reader:
+                global_path.x.append(float(line[0]))
+                global_path.y.append(float(line[1]))
+                global_path.k.append(float(line[2]))
+        return global_path
--- src/planner_and_control/script/lib/planner_utils/index_finder.py
+++ src/planner_and_control/script/lib/planner_utils/index_finder.py
@@ -1,1 +1,25 @@
-##must fix
+from math import hypot
+from planner_and_control.msg import Path
+from math import sqrt
+
+class IndexFinder:
+    def __init__(self, ego):
+        self.ego = ego
+        self.save_idx = 0
+        self.current_x = self.ego.x
+        self.current_y = self.ego.y
+        self.ego.index=0
+        self.min_dis=float('inf') # 무한대
+
+    def run(self):
+        for i in range(len(self.ego.path.x)) :
+            dx = self.current_y - self.ego.path.y[i]
+            dy = self.current_y - self.ego.path.y[i]
+            dis = sqrt(dx*dx + dy*dy)
+            if dis < min_dis :
+                min_dis=dis
+                self.ego.index = i
+
+        print(f"ego_index : {self.ego.index}")
+   
+        return self.ego.index
