78f6e8f1f9f403aebe3b26951ebe9a357c1b3e7e
HyunSwim <coldsz@naver.com>
2022-03-01 17:10:42 +0900

    Auto stash before merge of "Planning_and_Control" and "origin/Planning_and_Control"

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/hyunswim/TEAM-GIGACHA/src
-# Build directory: /home/hyunswim/TEAM-GIGACHA/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("planner_and_control")
--- devel/share/planner_and_control/cmake/planner_and_control-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(planner_and_control_MESSAGE_FILES "/home/hyunswim/TEAM-GIGACHA/src/planner_and_control/msg/Local.msg;/home/hyunswim/TEAM-GIGACHA/src/planner_and_control/msg/Path.msg;/home/hyunswim/TEAM-GIGACHA/src/planner_and_control/msg/Serial_Info.msg;/home/hyunswim/TEAM-GIGACHA/src/planner_and_control/msg/Control_Info.msg;/home/hyunswim/TEAM-GIGACHA/src/planner_and_control/msg/Ego.msg")
-set(planner_and_control_SERVICE_FILES "")
