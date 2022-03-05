4a1790c97104d6318cfbc1b8aa6f133dfaa1d764
HyunSwim <coldsz@naver.com>
2022-03-05 18:46:59 +0900

    Merge remote-tracking branch 'origin/LPP' into Planning_and_Control

--- devel/share/planner_and_control/cmake/planner_and_control-msg-extras.cmake
+++
@@ -1,2 +0,0 @@
-set(planner_and_control_MESSAGE_FILES "/home/inha/TEAM-GIGACHA/src/planner_and_control/msg/Local.msg;/home/inha/TEAM-GIGACHA/src/planner_and_control/msg/Gngga.msg;/home/inha/TEAM-GIGACHA/src/planner_and_control/msg/Path.msg;/home/inha/TEAM-GIGACHA/src/planner_and_control/msg/Serial_Info.msg;/home/inha/TEAM-GIGACHA/src/planner_and_control/msg/Control_Info.msg;/home/inha/TEAM-GIGACHA/src/planner_and_control/msg/Ego.msg")
-set(planner_and_control_SERVICE_FILES "")
