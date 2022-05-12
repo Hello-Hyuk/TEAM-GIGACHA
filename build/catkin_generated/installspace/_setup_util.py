dc45c9285bac1babb0a4cc4a347314aca32f9400
Nuvo <inhagigacha@gmail.com>
2022-05-12 16:45:02 +0900

    build complete

--- build/catkin_generated/installspace/_setup_util.py
+++ build/catkin_generated/installspace/_setup_util.py
@@ -268,11 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-<<<<<<< HEAD
-            CMAKE_PREFIX_PATH = r'/opt/ros/melodic'.split(';')
-=======
             CMAKE_PREFIX_PATH = r'/home/gigacha/TEAM-GIGACHA/devel;/opt/ros/melodic'.split(';')
->>>>>>> child_area
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
--- devel/share/e2box_imu/cmake/e2box_imuConfig.cmake
+++ devel/share/e2box_imu/cmake/e2box_imuConfig.cmake
@@ -154,11 +154,7 @@ foreach(library ${libraries})
     set(lib_path "")
     set(lib "${library}-NOTFOUND")
     # since the path where the library is found is returned we have to iterate over the paths manually
-<<<<<<< HEAD
-    foreach(path /home/gigacha/TEAM-GIGACHA/devel/lib;/opt/ros/melodic/lib)
-=======
     foreach(path /home/gigacha/TEAM-GIGACHA/devel/lib;/home/gigacha/TEAM-GIGACHA/devel/lib;/opt/ros/melodic/lib)
->>>>>>> child_area
       find_library(lib ${library}
         PATHS ${path}
         NO_DEFAULT_PATH NO_CMAKE_FIND_ROOT_PATH)
