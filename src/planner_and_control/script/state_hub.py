c77be8aa9e0df6d6ff1b295455433348e38fa344
HyunSwim <coldsz@naver.com>
2022-05-12 21:33:22 +0900

    plus updated state_hub code

--- src/planner_and_control/script/state_hub.py
+++ src/planner_and_control/script/state_hub.py
@@ -1,57 +1,59 @@
 import rospy
 from std_msgs.msg import String
-from planner_and_control.msg import Obj, Path, Control_Info
-#, Behavior_Info
+from planner_and_control.msg import Path, Control_Info, Ego, Perception
 
 class State_hub:
     def __init__(self):
-        rospy.init_node('state_hub', anonymous=False)
-        rospy.Subscriber('/obj', Obj, self.obj_callback) #sensor_hub
+        rospy.init_node('State_Hub', anonymous = False)
+
         rospy.Subscriber('/state', String, self.state_callback) #mission
-        rospy.Subscriber('/behavior', String, self.behavior_callback) #behavior
+        rospy.Subscriber('/behavior', String, self.behavior_callback)
+        rospy.Subscriber('/ego', Ego, self.ego_callback) #behavior
         rospy.Subscriber('/trajectory', Path, self.motion_callback) 
         rospy.Subscriber('/controller',Control_Info, self.control_callback)
-        self.control_msg = Control_Info()
-        #self.behavior = Behavior_Info()
-        self.obj = Obj()
-        self.motion = Path()
-        self.sign = ""
-        self.behavior = ""
+        rospy.Subscriber('/perception',Perception, self.perception_callback)
         self.state = ""
+        self.behavior = ""
+        self.motion = Path()
+        self.control_msg = Control_Info()
+        self.ego = Ego()
+        self.control = Control_Info()
+        self.perception = Perception()
 
-    def obj_callback(self, msg):  #sensor_hub callback
-        self.sign = msg
+    def ego_callback(self, msg):
+        self.ego = msg
 
     def state_callback(self, msg):  #mission_planner callback
         self.state = msg
 
-    def behavior_callback(self, msg): 
+    def behavior_callback(self, msg):  #behavior_planner
         self.behavior = msg
 
     def motion_callback(self, msg):
         self.motion = msg
 
     def control_callback(self, msg):
-        self.control_msg = msg
+        self.control = msg
 
-    
+    def perception_callback(self, msg):
+        self.perception = msg
 
     def run(self):
-        print("---sensor_hub--- ")
-        print(self.sign)
-        print("---mission_planner---")
-        print (self.state)
-        print("---behavior_planner---")
+        print("------------------------------------------------------")
+        print("\n[mission_planner]")
+        print(self.state)
+        print("\n[behavior_planner]")
         print(self.behavior)
-        print("---motion_planner--- : ")
-        #print(self.motion)
-        print("---controll_info---")
-        print(self.control_msg.speed)
-
+        #print("motion_planner : " + self.motion)
+        print("\n[controll_info]")
+        print(self.control)
+        print("\n[sing_name]")
+        print(self.perception.signname)
+        print("------------------------------------------------------")
 
 if __name__ == "__main__":
     A = State_hub()
-    rate = rospy.Rate(20)
+    rate = rospy.Rate(5)
     while not rospy.is_shutdown():
         A.run()
         rate.sleep
