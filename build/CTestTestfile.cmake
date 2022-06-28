70cdefae10ff7521cf8b60963b03abf3ba86a8d8
hello-hyuk <jay@inha.edu>
2022-06-28 19:11:46 +0900

    init narrow ws

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -1,9 +1,7 @@
 # CMake generated Testfile for 
-# Source directory: /home/gigacha/TEAM-GIGACHA/src
-# Build directory: /home/gigacha/TEAM-GIGACHA/build
+# Source directory: /home/jay/Dev/TEAM-GIGACHA/src
+# Build directory: /home/jay/Dev/TEAM-GIGACHA/build
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
-subdirs("planner_and_control")
-subdirs("e2box_AHRS")
--- build/gtest/CTestTestfile.cmake
+++ build/gtest/CTestTestfile.cmake
@@ -1,6 +1,6 @@
 # CMake generated Testfile for 
 # Source directory: /usr/src/googletest
-# Build directory: /home/gigacha/TEAM-GIGACHA/build/gtest
+# Build directory: /home/jay/Dev/TEAM-GIGACHA/build/gtest
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
