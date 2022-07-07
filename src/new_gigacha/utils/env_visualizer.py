6bcef19d9388a44be72953a539dc0c623268266a
HyunSwim <coldsz@naver.com>
2022-07-08 02:04:55 +0900

    add visualizer_others

--- src/new_gigacha/utils/env_visualizer.py
+++ src/new_gigacha/utils/env_visualizer.py
@@ -19,21 +19,21 @@ class Visualizer(threading.Thread):
         self.ego = parent.shared.ego
 
         self.global_path = self.shared.global_path # from localizer
-        # self.cut_path = self.shared.cut_path # from global path (find_local_path)
-        # self.lattice_path = self.shared.lattice_path # from LPP []
-        # self.local_path = self.lattice_path[self.shared.selected_lane]
+        self.cut_path = self.shared.cut_path # from global path (find_local_path)
+        self.lattice_path = self.shared.lattice_path # from LPP []
+        self.local_path = self.lattice_path[self.shared.selected_lane]
         
         # Publisher
         self.vis_global_path_pub = rospy.Publisher("/vis_global_path", Path, queue_size=1) # using path
-        # self.vis_local_path_pub = rospy.Publisher("/vis_local_path", Path, queue_size=1) # using path
-        # self.vis_lattice_path_0_pub = rospy.Publisher("/vis_lattice_path_0", Path, queue_size=1)
-        # self.vis_lattice_path_1_pub = rospy.Publisher("/vis_lattice_path_1", Path, queue_size=1)
-        # self.vis_lattice_path_2_pub = rospy.Publisher("/vis_lattice_path_2", Path, queue_size=1)
+        self.vis_local_path_pub = rospy.Publisher("/vis_local_path", Path, queue_size=1) # using path
+        self.vis_lattice_path_0_pub = rospy.Publisher("/vis_lattice_path_0", Path, queue_size=1)
+        self.vis_lattice_path_1_pub = rospy.Publisher("/vis_lattice_path_1", Path, queue_size=1)
+        self.vis_lattice_path_2_pub = rospy.Publisher("/vis_lattice_path_2", Path, queue_size=1)
         
-        # self.vis_trajectory_pub = rospy.Publisher("/vis_trajectory", PointCloud, queue_size=1)
-        # self.vis_pose_pub = rospy.Publisher("/vis_position", Odometry, queue_size=1)
+        self.vis_trajectory_pub = rospy.Publisher("/vis_trajectory", PointCloud, queue_size=1)
+        self.vis_pose_pub = rospy.Publisher("/vis_position", Odometry, queue_size=1)
         
-        # self.vis_obj_pub = rospy.Publisher("/vis_obj", MarkerArray, queue_size=1)
+        self.vis_obj_pub = rospy.Publisher("/vis_obj", MarkerArray, queue_size=1)
 
         # self.vis_trajectory_pub_dr = rospy.Publisher("/vis_trajectory_dr", PointCloud, queue_size=1)
         # self.vis_pose_pub_dr = rospy.Publisher("/vis_position_dr", Odometry, queue_size=1)
@@ -41,17 +41,26 @@ class Visualizer(threading.Thread):
         self.vis_global_path = Path() # using path
         self.vis_global_path.header.frame_id = "map"
         
-        # self.vis_local_path = Path() # using path
-        # self.vis_local_path.header.frame_id = "map"
+        self.vis_local_path = Path() # using path
+        self.vis_local_path.header.frame_id = "map"
         
-        # self.vis_trajectory = PointCloud()
-        # self.vis_trajectory.header.frame_id = "map"
+        self.vis_trajectory = PointCloud()
+        self.vis_trajectory.header.frame_id = "map"
+
+        self.vis_lattice_path_0 = Path()
+        self.vis_lattice_path_0.header.frame_id = "map"
+
+        self.vis_lattice_path_1 = Path()
+        self.vis_lattice_path_1.header.frame_id = "map"
+
+        self.vis_lattice_path_2 = Path()
+        self.vis_lattice_path_2.header.frame_id = "map"
 
         # self.vis_trajectory_dr = PointCloud()
         # self.vis_trajectory_dr.header.frame_id = "map"
         
-        # self.vis_pose = Odometry()
-        # self.vis_pose.header.frame_id = "map"
+        self.vis_pose = Odometry()
+        self.vis_pose.header.frame_id = "map"
 
         # self.vis_pose_dr = Odometry()
         # self.vis_pose_dr.header.frame_id = "map"
@@ -64,17 +73,17 @@ class Visualizer(threading.Thread):
             try:
                 ######################### POSE ##############################
                 
-                # ppoint = Point32()
-                # ppoint.x = self.ego.x
-                # ppoint.y = self.ego.y
-                # ppoint.z = 0
-                # self.vis_pose.pose.pose.position.x = ppoint.x
-                # self.vis_pose.pose.pose.position.y = ppoint.y
-                # self.vis_trajectory.header.stamp = rospy.Time.now()
-                # self.vis_pose.pose.pose.orientation = self.ego.orientation
-                # if self.t - time() < 0.5 :
-                #     self.t = time()
-                #     self.vis_trajectory.points.append(ppoint)
+                ppoint = Point32()
+                ppoint.x = self.ego.x
+                ppoint.y = self.ego.y
+                ppoint.z = 0
+                self.vis_pose.pose.pose.position.x = ppoint.x
+                self.vis_pose.pose.pose.position.y = ppoint.y
+                self.vis_trajectory.header.stamp = rospy.Time.now()
+                self.vis_pose.pose.pose.orientation = self.ego.orientation
+                if self.t - time() < 0.5 :
+                    self.t = time()
+                    self.vis_trajectory.points.append(ppoint)
 
                 # dead reckoning
                 # ppoint_dr = Point32()
@@ -119,94 +128,99 @@ class Visualizer(threading.Thread):
                 
                 #################################### LOCAL PATH ################################################
                 
-                # local_path = Path()
-                # for i in range(len(self.local_path.x)):
-                #     read_pose=PoseStamped()
-                #     read_pose.pose.position.x = self.local_path.x[i]
-                #     read_pose.pose.position.y = self.local_path.y[i]
-                #     read_pose.pose.position.z = 0
-                #     read_pose.pose.orientation.x=0
-                #     read_pose.pose.orientation.y=0
-                #     read_pose.pose.orientation.z=0
-                #     read_pose.pose.orientation.w=1
-                #     local_path.poses.append(read_pose)  
-                # self.vis_local_path.poses = local_path.poses
+                local_path = Path()
+                for i in range(len(self.local_path.x)):
+                    read_pose=PoseStamped()
+                    read_pose.pose.position.x = self.local_path.x[i]
+                    read_pose.pose.position.y = self.local_path.y[i]
+                    read_pose.pose.position.z = 0
+                    read_pose.pose.orientation.x=0
+                    read_pose.pose.orientation.y=0
+                    read_pose.pose.orientation.z=0
+                    read_pose.pose.orientation.w=1
+                    local_path.poses.append(read_pose)  
+                self.vis_local_path.poses = local_path.poses
 
                 #################################### LATTICE PATHS ################################################
 
-                # lattice_path_0 = Path()
-                # lattice_path_1 = Path()
-                # lattice_path_2 = Path()
-                # for i in range(len(self.lattice_path)):
-                #     for j in range(len(self.lattice_path[0].x)):
-                #         read_pose=PoseStamped()
-                #         read_pose.pose.position.x = self.lattice_path[i].x[j]
-                #         read_pose.pose.position.y = self.lattice_path[i].y[j]
-                #         read_pose.pose.position.z = 0
-                #         read_pose.pose.orientation.x=0
-                #         read_pose.pose.orientation.y=0
-                #         read_pose.pose.orientation.z=0
-                #         read_pose.pose.orientation.w=1
-                #         if i == 0:
-                #             lattice_path_0.poses.append(read_pose)
-                #         elif i == 1:
-                #             lattice_path_1.poses.append(read_pose)
-                #         elif i == 2:
-                #             lattice_path_2.poses.append(read_pose)
-                # self.vis_lattice_path_0.poses = lattice_path_0.poses
-                # self.vis_lattice_path_1.poses = lattice_path_1.poses
-                # self.vis_lattice_path_2.poses = lattice_path_2.poses
+                lattice_path_0 = Path()
+                lattice_path_1 = Path()
+                lattice_path_2 = Path()
+                for i in range(len(self.lattice_path)):
+                    for j in range(len(self.lattice_path[0].x)):
+                        read_pose=PoseStamped()
+                        read_pose.pose.position.x = self.lattice_path[i].x[j]
+                        read_pose.pose.position.y = self.lattice_path[i].y[j]
+                        read_pose.pose.position.z = 0
+                        read_pose.pose.orientation.x=0
+                        read_pose.pose.orientation.y=0
+                        read_pose.pose.orientation.z=0
+                        read_pose.pose.orientation.w=1
+                        if i == 0:
+                            lattice_path_0.poses.append(read_pose)
+                        elif i == 1:
+                            lattice_path_1.poses.append(read_pose)
+                        elif i == 2:
+                            lattice_path_2.poses.append(read_pose)
+                self.vis_lattice_path_0.poses = lattice_path_0.poses
+                self.vis_lattice_path_1.poses = lattice_path_1.poses
+                self.vis_lattice_path_2.poses = lattice_path_2.poses
 
 
                 #############################object#########################################
                 
-                # vis_obj = MarkerArray()
-                # c_id = 0
-                
-                # for i in range(len(self.perception.objx)):
-                #     circle_marker = Marker()
-                #     circle_marker.header.frame_id = "map"
-                #     circle_marker.header.stamp = rospy.Time.now()
-                #     circle_marker.ns = "circles"
-                #     circle_marker.id = c_id
-                #     circle_marker.type = Marker.CYLINDER
-                #     circle_marker.action = Marker.ADD
-                #     circle_marker.pose.position.z = -0.1
-                #     circle_marker.pose.orientation.x = 0.0
-                #     circle_marker.pose.orientation.y = 0.0
-                #     circle_marker.pose.orientation.z = 0.0
-                #     circle_marker.pose.orientation.w = 1.0
-                #     circle_marker.scale.z = 0.1
-                #     circle_marker.color.r = 0.2
-                #     circle_marker.color.g = 0.8
-                #     circle_marker.color.b = 0.2
-                #     circle_marker.color.a = 1.0
-                #     circle_marker.lifetime = rospy.Duration(0.1)
-                #     circle_marker.pose.position.x = self.perception.objx[i]
-                #     circle_marker.pose.position.y = self.perception.objy[i]
-                #     circle_marker.scale.x = 2.0 * self.perception.objr[i]
-                #     circle_marker.scale.y = 2.0 * self.perception.objr[i]
-                #     vis_obj.markers.append(circle_marker)
-                #     c_id = c_id + 1
+                vis_obj = MarkerArray()
+                c_id = 0
+                for i in range(len(self.perception.objx)):
+                    circle_marker = Marker()
+                    circle_marker.header.frame_id = "map"
+                    circle_marker.header.stamp = rospy.Time.now()
+                    circle_marker.ns = "circles"
+                    circle_marker.id = c_id
+                    circle_marker.type = Marker.CYLINDER
+                    circle_marker.action = Marker.ADD
+                    circle_marker.pose.position.z = -0.1
+                    circle_marker.pose.orientation.x = 0.0
+                    circle_marker.pose.orientation.y = 0.0
+                    circle_marker.pose.orientation.z = 0.0
+                    circle_marker.pose.orientation.w = 1.0
+                    circle_marker.scale.z = 0.1
+                    circle_marker.color.r = 0.2
+                    circle_marker.color.g = 0.8
+                    circle_marker.color.b = 0.2
+                    circle_marker.color.a = 1.0
+                    circle_marker.lifetime = rospy.Duration(0.1)
+                    circle_marker.pose.position.x = self.perception.objx[i]
+                    circle_marker.pose.position.y = self.perception.objy[i]
+                    circle_marker.scale.x = 2.0 * self.perception.objr[i]
+                    circle_marker.scale.y = 2.0 * self.perception.objr[i]
+                    vis_obj.markers.append(circle_marker)
+                    c_id = c_id + 1
                     
-                # # publish
-                # self.vis_obj_pub.publish(vis_obj)
+                # publish
+                self.vis_obj_pub.publish(vis_obj)
                 
                 self.vis_global_path.header.stamp = rospy.Time.now()
                 self.vis_global_path_pub.publish(self.vis_global_path)
                 
-                # self.vis_local_path.header.stamp = rospy.Time.now()
-                # self.vis_local_path_pub.publish(self.vis_local_path)
+                self.vis_local_path.header.stamp = rospy.Time.now()
+                self.vis_local_path_pub.publish(self.vis_local_path)
 
-                # self.vis_lattice_path_0_pub.publish(self.vis_lattice_path_0)
-                # self.vis_lattice_path_1_pub.publish(self.vis_lattice_path_1)
-                # self.vis_lattice_path_2_pub.publish(self.vis_lattice_path_2)
+                self.vis_lattice_path_0.header.stamp = rospy.Time.now()
+                self.vis_lattice_path_0_pub.publish(self.vis_lattice_path_0)
 
-                # self.vis_trajectory_pub.publish(self.vis_trajectory)
+                self.vis_lattice_path_1.header.stamp = rospy.Time.now()
+                self.vis_lattice_path_1_pub.publish(self.vis_lattice_path_1)
+
+                self.vis_lattice_path_2.header.stamp = rospy.Time.now()
+                self.vis_lattice_path_2_pub.publish(self.vis_lattice_path_2)
+
+                self.vis_trajectory_pub.publish(self.vis_trajectory)
+
                 # self.vis_trajectory_pub_dr.publish(self.vis_trajectory_dr)
 
-                # self.vis_pose.header.stamp = rospy.Time.now()
-                # self.vis_pose_pub.publish(self.vis_pose)
+                self.vis_pose.header.stamp = rospy.Time.now()
+                self.vis_pose_pub.publish(self.vis_pose)
                 # self.vis_pose_pub_dr.publish(self.vis_pose_dr)
             except IndexError:
                 print("+++++++++++++++++")
