8ca31aec9ea26ddf750c3427685d780247ae9d3a
hyunswim <coldsz@naver.com>
2022-07-07 18:06:52 +0900

    modify perception.py

--- src/new_gigacha/shared/perception.py
+++ src/new_gigacha/shared/perception.py
@@ -1,10 +1,12 @@
 import rospy
 from planner_and_control.msg import Perception
+from visualization_msgs.msg import MarkerArray, Marker
 
 class Perception_():
    def __init__(self):
 
       rospy.Subscriber("/input", Perception, self.input_callback)
+      rospy.Subscriber("/lidar_topic", MarkerArray, self.lidar_callback)
 
       self.signx = []
       self.signy = []
@@ -30,3 +32,21 @@ class Perception_():
       self.tleft = msg.tleft
       self.tgreen = msg.tgreen
       self.signname = msg.signname
+
+   def lidar_callback(self, msg):
+      tmp_objx = []
+      tmp_objy = []
+      tmp_objw = []
+      tmp_objh = []
+
+      for i in range(len(msg.markers)):
+         tmp_objx.append(msg.markers[i].pose.position.x)
+         tmp_objy.append(msg.markers[i].pose.position.y)
+         tmp_objw.append(msg.markers[i].scale.x)
+         tmp_objh.append(msg.markers[i].scale.y)
+      
+      self.objx = tmp_objx
+      self.objy = tmp_objy
+      self.objw = tmp_objw
+      self.objh = tmp_objh
+      
