7d83278e725bf9a8840c3de2d915623c19ee6b0f
yookyung <oyk0000@naver.com>
2022-07-13 11:14:48 +0900

    add input node

---
+++ src/semi_final_pkg/utils/input.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+import rospy
+from .sig_int_handler import ActivateSignalInterruptHandler
+from planner_and_control.msg import Perception
+
+class Input:
+    def __init__(self):
+        rospy.init_node('Input', anonymous = False)
+        self.pub = rospy.Publisher("/input", Perception, queue_size = 1)
+        self.perception = Perception()
+        self.perception.objx = []####
+        self.perception.objy = []####
+        self.perception.objr = []####
+        
+    def run(self):
+        self.pub.publish(self.perception)
+
+if __name__ == "__main__":
+    ActivateSignalInterruptHandler()
+    ss = Input()
+    rate = rospy.Rate(1)
+    while not rospy.is_shutdown():
+        ss.run()
+        rate.sleep()
