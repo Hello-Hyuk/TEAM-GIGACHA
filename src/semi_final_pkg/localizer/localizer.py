5896487e69397c34827335fadefe942793785f06
HyunSwim <coldsz@naver.com>
2022-07-17 17:30:16 +0900

    add curvature and yaw to path list
    add codes shows how to get curvature and yaw value from path

--- src/semi_final_pkg/localizer/localizer.py
+++ src/semi_final_pkg/localizer/localizer.py
@@ -2,8 +2,7 @@
 import time
 import csv
 import threading
-import math
-from math import hypot
+from math import atan2, sqrt
 from time import sleep
 
 from localizer.ahrs import IMU
@@ -17,11 +16,33 @@ class Localizer(threading.Thread):
         self.period = 1.0 / rate
 
         self.ego = parent.shared.ego
+        self.shared = parent.shared
+        self.global_path = parent.shared.global_path
 
         self.gps = GPS()
         self.imu = IMU()
 
         self.offset = 0
+        self.switch = True
+
+    def add_curvature_yaw(self):
+        for i in range(len(self.global_path.x)-1):
+            yaw = atan2(self.global_path.y[i+1]-self.global_path.y[i],\
+            self.global_path.x[i+1]-self.global_path.x[i])
+            self.global_path.yaw_list.append(yaw) # enu coordinate
+
+            # calculate curvature
+            center_point_x = (self.global_path.x[i-1] + self.global_path.x[i+1]) / 2
+            center_point_y = (self.global_path.y[i-1] + self.global_path.y[i+1]) / 2
+
+            w = sqrt((self.global_path.x[i+1] - self.global_path.x[i-1])**2\
+            + (self.global_path.y[i+1] - self.global_path.y[i-1])**2)
+            h = sqrt((self.global_path.x[i] - center_point_x)**2\
+            + (self.global_path.y[i] - center_point_y)**2)
+
+            radius = (h / 2) + (pow(w,2) / 8*h)
+
+            self.global_path.curvature.append(1 / radius)
 
     def heading_decision(self):
         global time_sync
@@ -41,6 +62,10 @@ class Localizer(threading.Thread):
 
     def run(self):
         while True:
+            if self.shared.state == "2nd" and self.switch:
+                self.add_curvature_yaw()
+                self.switch = False
+
             self.heading_decision()
             self.ego.x = self.gps.x
             self.ego.y = self.gps.y
--- src/semi_final_pkg/shared/path.py
+++ src/semi_final_pkg/shared/path.py
@@ -3,3 +3,5 @@ class Path():
         self.x = []
         self.y = []
         self.steer_list = []
+        self.curvature = []
+        self.yaw_list = []
