cf45fa670d0ab0674c4372ba6a015de0ed9e3283
Nuvo <inhagigacha@gmail.com>
2022-08-17 18:52:07 +0900

    delete build, devel forder to merge

--- build/new_gigacha/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gigacha/TEAM-GIGACHA/src/new_gigacha
-# Build directory: /home/gigacha/TEAM-GIGACHA/build/new_gigacha
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- devel/share/new_gigacha/cmake/new_gigacha-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(new_gigacha_MSG_INCLUDE_DIRS "/home/gigacha/TEAM-GIGACHA/src/new_gigacha/msg")
-set(new_gigacha_MSG_DEPENDENCIES std_msgs;geometry_msgs;new_gigacha;local_pkg)
