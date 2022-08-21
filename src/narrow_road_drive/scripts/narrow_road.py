462678c15858909973270a85466796ece4cbd293
cheon <cjy7050@gmail.com>
2022-08-21 17:09:38 +0900

    test

--- src/narrow_road_drive/scripts/narrow_road.py
+++ src/narrow_road_drive/scripts/narrow_road.py
@@ -6,6 +6,7 @@ import rospy
 from geometry_msgs.msg import Point
 from visualization_msgs.msg import MarkerArray, Marker
 import itertools as it
+import numpy as np
 
 class path_maker:
     def __init__(self):
@@ -30,6 +31,50 @@ class path_maker:
         self.target_point_vis = Marker()
 
         rospy.spin()
+
+    def cal_circul(self,points):
+
+    #     for point in points:
+    #         cx=point[0]
+    #         cy=point[1]
+
+    #         A=np.array([2*cx,2*cy,1])
+    #         B=np.array([cx*cx+cy*cy])
+        
+    #         np.append(A, A, axis=0)
+    #         np.append(B, B, axis=0)
+
+    #     C=np.linalg.solve(A,B)
+    #     print(C)
+
+        x_list=[]
+        y_list=[]
+
+         
+        for point in points:
+            x=point[0]
+            y=point[1]
+            x_list.append([-2*x,-2*y,1])
+            y_list.append(-(x*x)-(y*y))
+            
+        x_matrix=np.array(x_list)
+        y_matrix=np.array(y_list)
+        x_trans=x_matrix.T
+            
+
+        a_matrix=np.linalg.inv(x_trans.dot(x_matrix)).dot(x_trans).dot(y_matrix)
+        a=a_matrix[0]
+        b=a_matrix[1]
+        c=a_matrix[2]
+        r=sqrt(a*a+b*b-c)
+        print(r)
+        self.target_point.x = a
+        self.target_point.y = b
+
+
+
+
+
 
 
     def calc_distance(self, obs_x, obs_y):
@@ -80,98 +125,116 @@ class path_maker:
 
 
 
+
+    def secondCOM(self, points):
+        x_sum = 0
+        y_sum = 0
+
+
+
+        if points[0][1]>0 and points[1][1]>0:
+            print("X")
+            self.target_point.x=point[1][0]
+            self.target_point.y=point[1][1]+1
+
+
+        elif points[0][1]<0 and points[1][1]<0:
+            print("XX")
+            self.target_point.x=point[1][0]
+            self.target_point.y=point[1][1]-1
+        else:
+
+            for point in points:
+                x_sum = x_sum + point[0]
+                y_sum = y_sum + point[1]
+
+            self.target_point.x = x_sum / len(points)
+            self.target_point.y = y_sum / len(points)
+
+
+
+       
+
+
+
+
+
+    def tripleCOM(self, points):
+    
+        
+        x_sum = 0
+        y_sum = 0
+        for point in points:
+            x_sum = x_sum + point[0]
+            y_sum = y_sum + point[1]
+
+        self.target_point.x = x_sum / (len(points)+1)
+        self.target_point.y = y_sum / (len(points)+1)          
+
+
+
 #method1
-    # def makeCOM(self, points):
-    #     x_sum = 0
-    #     y_sum = 0
-    #     tmp1=0
-    #     tmp2=0
-
-    #     for point in points:
-
-    #         if point[1]<0:
-    #             tmp1=tmp1+1
-    #             if tmp1>=3:
-    #                 pass
-
-    #             else:
-    #                 x_sum = x_sum + point[0]
-    #                 y_sum = y_sum + point[1]
-
-
-    #         elif point[1]>0:
-    #             tmp2=tmp2+1
-    #             if tmp2>=3:
-    #                 pass
-
-    #             else:
-    #                 x_sum = x_sum + point[0]
-    #                 y_sum = y_sum + point[1]    
-
-    #     self.target_point.x = x_sum / len(points)
-    #     self.target_point.y = y_sum / len(points)
-
-
-# method2
-    def makeCOM(self, points):
+    def fouthCOM(self, points):
+       
         x_sum = 0
         y_sum = 0
         tmp1=0
         tmp2=0
-        point_L=[]
         point_R=[]
-
-
+        point_L=[]
 
         for point in points:
 
             if point[1]<0:
-                point_L = point
-                point_R=0
+                point_R=point
+
 
             if point[1]>0:
-                point_R = point
-                point_L=0
-    
+                point_L=point
+
+
+
+
             if point[1]<0:
                 tmp1=tmp1+1
                 if tmp1>=3:
-                    x_sum = x_sum + point_L[0]+3*point_R[0]
-                    y_sum = y_sum + point_L[1]+3*point_R[0]
 
+                    x_sum = x_sum + point[0]+3*point_L[0]
+                    y_sum = y_sum + point[1]+3*point_L[1]
 
-                    
+                    self.target_point.x = x_sum /6
+                    self.target_point.y = y_sum /6
 
                 else:
-                    x_sum = x_sum + point_L[0]+point_R[0]
-                    y_sum = y_sum + point_L[0]+point_R[0]
+                    x_sum = x_sum + point[0]
+                    y_sum = y_sum + point[1]
+
+                    self.target_point.x = x_sum / len(points)
+                    self.target_point.y = y_sum / len(points)
 
 
             elif point[1]>0:
                 tmp2=tmp2+1
+                
                 if tmp2>=3:
-                    x_sum = x_sum + 3*point_L[0]+point_R[0]
-                    y_sum = y_sum + 3*point_L[1]+point_R[0]
+                    x_sum = x_sum + point[0]+3*point_R[0]
+                    y_sum = y_sum + point[1]+3*point_R[1]
 
+                    self.target_point.x = x_sum /6
+                    self.target_point.y = y_sum / 6
 
                 else:
-                    x_sum = x_sum + point_L[0]+point_R[0]
-                    y_sum = y_sum + point_L[0]+point_R[0]
+                    x_sum = x_sum + point[0]
+                    y_sum = y_sum + point[1]
 
-
-
-
-
-
-        self.target_point.x = x_sum / len(points)
-        self.target_point.y = y_sum / len(points)
+                    self.target_point.x = x_sum / len(points)
+                    self.target_point.y = y_sum / len(points)
 
 
 
 
     def makePath(self, msg):
-        # print(type(msg))
-        # 각 장애물의 중앙점 따로 저장-> obstacles의 중앙점, 거리 값 같이 계산
+
         
         for obs in msg.markers:
             tmp_obs_dis = self.calc_distance(obs.pose.position.x, obs.pose.position.y)
@@ -187,32 +250,22 @@ class path_maker:
             pass
         elif len(self.obstacles) == 1:
             self.singleCOM(self.obstacles)
+
+        elif len(self.obstacles) == 2:
+            self.secondCOM(self.obstacles)
+        
+        elif len(self.obstacles) == 3:
+            self.cal_circul(self.obstacles)
+            #self.tripleCOM(self.obstacles)
+
+
         else:
-            self.makeCOM(self.obstacles)
+            self.cal_circul(self.obstacles)
+            #self.fouthCOM(self.obstacles)
+
         print("self.obstacles",self.obstacles)
    
-
-    # def makePath(self, msg):
-    #     # print(type(msg))
-    #     # 각 장애물의 중앙점 따로 저장-> obstacles의 중앙점, 거리 값 같이 계산
-        
-    #     for obs in msg.markers:
-    #         tmp_obs_dis = self.calc_distance(obs.pose.position.x, obs.pose.position.y)
-    #         self.obstacles.append([round(obs.pose.position.x, 2), round(obs.pose.position.y, 2), round(tmp_obs_dis, 2)])
-        
-    #     # 점 정렬 & 4개 점 filtering
-    #     self.obstacles.sort(key = lambda x : x[2])
-    #     if len(self.obstacles) > 4:
-    #         self.obstacles = self.obstacles[0:4]    # 가장 거리가 짧은 4개의 점만 추출
-
-
-    #     if len(self.obstacles) == 0:
-    #         pass
-    #     elif len(self.obstacles) == 1:
-    #         self.singleCOM(self.obstacles)
-    #     else:
-    #         self.makeCOM(self.obstacles)
-    #     print("self.obstacles",self.obstacles)    
+  
 
         self.makeVisual(self.target_point)
         self.vis_pub.publish(self.target_point_vis)
