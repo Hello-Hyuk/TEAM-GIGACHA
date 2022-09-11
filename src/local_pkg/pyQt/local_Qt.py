15452f1c3a4623b129e9013ea58484b512d5cb8f
HyunSwim <coldsz@naver.com>
2022-09-12 03:45:16 +0900

    master gui

--- src/local_pkg/pyQt/local_Qt.py
+++ src/local_pkg/pyQt/local_Qt.py
@@ -3,6 +3,7 @@ import rospy
 from local_pkg.msg import Local
 from local_pkg.msg import Serial_Info
 from local_pkg.msg import Control_Info
+from local_pkg.msg import Master
 from geometry_msgs.msg import Pose
 from sensor_msgs.msg import NavSatFix 
 from ublox_msgs.msg import NavPVT 
@@ -13,6 +14,10 @@ from PyQt5 import uic
 
 form_class = uic.loadUiType("uis/local_ui.ui")[0]
 
+global checker_list
+checker_list = [False, False, False]
+
+
 class WindowClass(QDialog, form_class):
     def __init__(self):
         super().__init__()
@@ -21,6 +26,7 @@ class WindowClass(QDialog, form_class):
 
         rospy.init_node("Gigacha_ui", anonymous = False)
         rospy.Subscriber("/local_msgs", Local, self.localCallback)
+        rospy.Subscriber("/from_master", Master, self.masterCallback)
         rospy.Subscriber("ublox_gps/fix", NavSatFix, self.gpsCallback) 
         rospy.Subscriber("ublox_gps/navpvt", NavPVT, self.navpvtCallback) 
         rospy.Subscriber("/serial", Serial_Info, self.serialCallback)
@@ -35,6 +41,25 @@ class WindowClass(QDialog, form_class):
 
         self.slider_speed.setMaximum(20)
         self.slider_speed.setMinimum(0)
+
+        self.rbtn_localizer.setAutoExclusive(False)
+        self.rbtn_mission_pln.setAutoExclusive(False)
+        self.rbtn_behavior_pln.setAutoExclusive(False)
+        self.rbtn_motion_pln.setAutoExclusive(False)
+        self.rbtn_lat_con.setAutoExclusive(False)
+        self.rbtn_lon_con.setAutoExclusive(False)
+
+    def masterCallback(self, msg):
+        self.lbl_index.setText(str(msg.index))
+        self.lbl_mission.setText(msg.mission)
+        self.lbl_behavior.setText(msg.behavior)
+
+        self.rbtn_localizer.setChecked(msg.localizer)
+        self.rbtn_mission_pln.setChecked(msg.mission_pln)
+        self.rbtn_behavior_pln.setChecked(msg.behavior_pln)
+        self.rbtn_motion_pln.setChecked(msg.motion_pln)
+        self.rbtn_lat_con.setChecked(msg.lat_con)
+        self.rbtn_lon_con.setChecked(msg.lon_con)
 
     def localCallback(self, msg):
         self.lbl_x.setText(str(msg.x))
@@ -103,7 +128,7 @@ class WindowClass(QDialog, form_class):
     def initUI(self):
         self.setWindowTitle('Local Informations')
         self.setWindowIcon(QIcon("location.png"))
-        self.setGeometry(300, 300, 600, 560)
+        self.setGeometry(300, 300, 700, 550)
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv)
